6567871a3931be00cd804d1a0f49eca123c09969
jyothish2312 <42204656+jyothish2312@users.noreply.github.com>
2020-06-21 14:27:35 +0530

    Add files via upload

---
+++ DispenserCode V2_Beta.ino
@@ -0,0 +1,180 @@
+//IN THIS VERSION, WE HAVE ADDED A 7 SEGMENT DISPLAY TO TAKE THE READING OF THE COUNTER WHEN REQUIRED.
+//THIS VERSION IS STILL UNDER DEVELOPMENT, AND THERE MIGHT BE BUGS.
+
+//AIM 1: Dispenser should be manually operatable if required.
+//AIM 2: Amount of the liquid being dispensed in automatic mode must be controlable.
+//AIM 3: A log of the dispensed amount should be maintained automatically.
+//Machine uses an Arduino NANO for storing the data and operating the machine.
+#include <EEPROM.h>
+
+//Pump activation, sensor, RGB LEDs and Reset button are wired according to the following allocation: 
+int pumpin=13; // the pin designated for activation of the pump
+int sensorpin = 12;  //arduino pin the sensor is plugged to
+int readpin=11; // to activate reader pin
+int resetpin = 10; // button to activate counter reset function : as per code, it'll be activated if button is contineously pressed for more then 3 seconds
+//pins 2 to 9 are dedicated for the 7 segment display.
+
+int regpin = A0;  //pin which is connected to potentiometer- designated to control the time of pump activation for dispensing.
+int red = A1;    //red led pin : designated to illuminate when memory reset function is activated.
+int green = A2; //green led pin : designated to illuminate when dispensing is activated
+int blue = A3;  //blue led pin : designated to blink in standby state
+
+int counter=0;  //total time for which motor HAS dispensed the liquid so far.
+int rate= 5; //rate of flow for the pump in Ml/Sec
+int potval; //value of the potentiometer
+int maxtime=2500; //the maximum caliberatable activation time for each pump cycle
+int timer;//timer is the pump activation time set using the potentiometer connected to regpin: min time is 10 milimeter,max time is 2.5 seconds right now.
+
+ 
+
+//the following chart is for the coding of a 7 segment led display which will display counter when required
+bool statepan[12][7]={{1,1,1,1,1,1,0}, //0   //code is based on the circuit discussed at: https://www.allaboutcircuits.com/projects/interface-a-seven-segment-display-to-an-arduino/
+                     {0,1,1,0,0,0,0},  //1
+                     {1,1,0,1,1,0,1},  //2
+                     {1,1,1,1,0,0,1},  //3
+                     {0,1,1,0,0,1,1},  //4
+                     {1,0,1,1,0,1,1},  //5
+                     {1,0,1,1,1,1,1},  //6
+                     {1,1,1,0,0,0,0},  //7
+                     {1,1,1,1,1,1,1},  //8
+                     {1,1,1,1,0,1,1},  //9
+                     {0,0,0,0,0,0,1},  //-
+                     {0,0,0,0,0,0,1}}; //off.
+
+//********************************************************************************************************************
+//********************************************************************************************************************
+
+void setup() {
+  EEPROM.get(0, counter); // obtain the last value of total dispense time from EEPROMs first memory address into counter variable.
+  pinMode(sensorpin, INPUT); 
+  
+  for(int n=0; n<<7; n++){
+  pinMode(n, OUTPUT);
+  }
+  pinMode(13, OUTPUT);
+  pinMode(12, INPUT);
+  pinMode(11, INPUT);
+  pinMode(10, INPUT);
+  
+  digitalWrite(pumpin, LOW);//pump is set to it's off state
+  Serial.begin(9600);   //serial communication is started at frequency 9600
+ }
+
+void loop() {
+   //we'll start with reading the potentiometer value
+    potval=(analogRead(regpin));//value of potentiometer is read, to set the active time for pump per stimulus
+    timer= ((maxtime/1023)*potval);
+    analogWrite(blue,1023);     //in standby state, the indicator is blue
+    analogWrite(green,0);     //green is off right now. blue will be replaced with green when machine is activated
+    analogWrite(red, 0);      //red indicator is off right now, it'll be on when the reset function is activated.
+//STEP 1: Loop initialisation
+     //STEP 1-A: Start the program by an option to reset the counter. if someone holds the reset button for 3 seconds, the counter resets
+       if((debounce(resetpin))==HIGH){
+          reset();
+        }
+
+       if((debounce(readpin))==HIGH){
+          readfunction(counter);
+        }
+  
+     // STEP 1-B: with every loop, the information about the current statistics is printed in the serial monitor
+        Serial.print("Current value of counter is: ");
+        Serial.println(counter);
+      //  Serial.print("and amount of sanitizer dispensed in mililiter is approximately =  ");
+        //Serial.println((counter*rate));
+        Serial.print("current value of pump activation time (In Mili Seconds) is");
+        Serial.println((timer));
+        
+        delay(100);
+//*********************initialisation complete*****************************************************
+  
+
+//STEP 2: the sensor status is checked and if activated, dispensor is activated 
+      //STEP 2-A: sensor is checked
+         if((digitalRead(sensorpin))== HIGH){
+      //STEP 2-B: necessary indicators are given/changed
+            analogWrite(blue,0);
+            analogWrite(green,1023);
+      //STEP 2-C: pump is activated to begin the dispensing.
+            digitalWrite(pumpin, HIGH);
+            delay(timer);
+            digitalWrite(pumpin, LOW);
+     // STEP 2-D: conter value is increased and EEPROM value is set to counter value.
+            counter = counter+timer;
+            EEPROM.put(0, counter);
+     //STEP 2-E: necessary indicators are given/changed       
+            analogWrite(green,0);
+            analogWrite(blue,1023);
+         }
+//*******************************Action  completed**************************************************
+        
+        else{
+        //in lack of an activation from sensor, always pull the pump to off position.
+          digitalWrite(pumpin, LOW);
+         
+        }
+         delay(200);
+         analogWrite(blue,0);
+         delay(300);
+}
+
+
+//######################-All the functions called in the loop are listed below-####################################
+
+//FUNCTION 1: Sometimes due to electrical disturbances, there maybe registeration of stray activation of a push switch
+//debounce function is used to verify any button inputs; as is required for the counter reset function.
+bool debounce(int pin){
+  int A = digitalRead(pin);
+  delay(30);                // this delay makes it possible to take two readings at 30 milisecond gap for verification of a desired activation of the button
+  int B = digitalRead(pin);
+  if(A == B){return A;}
+  else{debounce(resetpin);}
+  }
+
+
+
+//FUNCTION 2: reset function will reset the counter and 0 address of the EEPROM to 0 when called for
+void reset(){
+  analogWrite(blue, 0);
+  analogWrite(red, 1023);
+  delay(3000);    // it is required to hold the button for 3 seconds for the reset function to begin it's job
+  // after 3 seconds, another reading is taken, which, if matched HIGH, will reset the values
+  if((debounce(resetpin)) == 1){ 
+       analogWrite(red, 0);
+       EEPROM.put(0,0);
+       EEPROM.get(0,counter);
+       delay(1000);
+     }
+  
+  analogWrite(blue, 1023);
+  }
+
+ //FUNCTION 3: this function will display the counter on a 7 segment display when activated with a button.
+  void readfunction(long data){
+  leddisplay(11);
+  delay(1000);
+  int arr[12];
+  int i=11;
+  while(i>=0){
+    arr[(11-(i))]= (long(data/((pow(10,i))))) - (long(data/((pow(10,(i+1)))))*10);
+    leddisplay((arr[(19-i)]));
+    delay(1000);
+    i--;
+  }
+  leddisplay(11);
+  delay(1000);
+  leddisplay(12);
+  }
+
+
+
+//FUNCTION 4: this function operates the 7 segment display.
+
+  void leddisplay(int k){
+  int l=2;
+  while(l <<9){
+  digitalWrite(l, (!(statepan[k][(l-2)])));
+  }
+  }
+
+ 
